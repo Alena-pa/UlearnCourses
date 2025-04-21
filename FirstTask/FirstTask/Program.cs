c41362252f11657ae483ea3f602e5deddd92a964
Alena-pa <alena.pakhnusheva@gmail.com>
2025-04-21 14:11:52 +0300

    idk

---
+++ FirstTask/FirstTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Programm
+{
+    static void Main()
+    {
+        int summ = 0;
+        for (int i = 0; i < 1000; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                summ += i;
+            }
+        }
+        Console.WriteLine(summ);
+    }
+}
---
+++ SecondTask/SecondTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int hours = 1;
+        int minutes = 30;
+        hours = hours % 12;
+        double hourAngle = 30 * hours + 0.5 * minutes;
+        double minuteAngle = 6 * minutes;
+
+        double angle = Math.Abs(hourAngle - minuteAngle);
+        double answer = Math.Min(angle, 360 - angle);
+
+        Console.WriteLine(answer);
+    }
+}
