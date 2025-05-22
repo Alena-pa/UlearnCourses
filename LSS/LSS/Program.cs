90acd0739cf1320bf7b3961ab41f067c441ce58d
Alena-pa <alena.pakhnusheva@gmail.com>
2025-05-22 17:18:06 +0300

    Add LSS

---
+++ LSS/LSS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class LimitedSizeStack<T>
+{
+    private readonly LinkedList<T> items;
+    private readonly int maxSize;
+
+    public LimitedSizeStack(int undoLimit)
+    {
+        items = new LinkedList<T>();
+        maxSize = Math.Max(0, undoLimit);
+    }
+
+    public void Push(T item)
+    {
+        if (maxSize == 0) return;
+
+        if (items.Count >= maxSize)
+            items.RemoveFirst(); // O(1)
+
+        items.AddLast(item); // O(1)
+    }
+
+    public T Pop()
+    {
+        if (items.Count == 0)
+            throw new InvalidOperationException("Stack is empty");
+
+        T item = items.Last.Value;
+        items.RemoveLast();
+        return item;
+    }
+
+    public int Count => items.Count;
+}
