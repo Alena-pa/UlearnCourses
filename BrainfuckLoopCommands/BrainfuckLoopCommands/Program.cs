081e4a00aaf13477dbbf8fb0a329366e8cbec3a8
Alena-pa <alena.pakhnusheva@gmail.com>
2025-05-22 17:30:00 +0300

    Add  BrainfuckLoopCommands

---
+++ BrainfuckLoopCommands/BrainfuckLoopCommands/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace func.brainfuck
+{
+    public class BrainfuckLoopCommands
+    {
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            var loopsByStart = new Dictionary<int, int>();
+            var currentLoopsByStart = new Stack<int>();
+
+            vm.RegisterCommand('[', b => HandleLoopStart(b, loopsByStart, currentLoopsByStart));
+            vm.RegisterCommand(']', b => HandleLoopEnd(b, currentLoopsByStart));
+        }
+
+        private static void HandleLoopStart(
+        IVirtualMachine vmState,
+        Dictionary<int, int> loopsByStart,
+        Stack<int> currentLoopsByStart)
+        {
+            if (loopsByStart.Count == 0)
+                PrecomputeLoops(vmState, loopsByStart);
+
+            if (vmState.Memory[vmState.MemoryPointer] == 0)
+                vmState.InstructionPointer = loopsByStart[vmState.InstructionPointer];
+            else
+                currentLoopsByStart.Push(vmState.InstructionPointer);
+        }
+
+
+        private static void HandleLoopEnd(IVirtualMachine vmState, Stack<int> currentLoopsByStart)
+        {
+            if (vmState.Memory[vmState.MemoryPointer] != 0)
+                vmState.InstructionPointer = currentLoopsByStart.Peek();
+            else
+                currentLoopsByStart.Pop();
+        }
+
+        private static void PrecomputeLoops(IVirtualMachine vmState, Dictionary<int, int> loopsByStart)
+        {
+            var loopStack = new Stack<int>();
+
+            for (int i = 0; i < vmState.Instructions.Length; i++)
+            {
+                char instruction = vmState.Instructions[i];
+                if (instruction == '[')
+                    loopStack.Push(i);
+                else if (instruction == ']')
+                {
+                    int start = loopStack.Pop();
+                    loopsByStart[start] = i;
+                }
+            }
+        }
+    }
+}
