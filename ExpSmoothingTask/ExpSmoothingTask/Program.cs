baaf7fb57cf77083c99624684bd504f7e300dda4
Alena-pa <alena.pakhnusheva@gmail.com>
2025-05-22 17:24:52 +0300

    add ExpSmoothingTask

---
+++ ExpSmoothingTask/ExpSmoothingTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace yield;
+
+public static class ExpSmoothingTask
+{
+    public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
+    {
+        bool first = true;
+        double prevSmoothedY = 0;
+
+        foreach (var point in data)
+        {
+            double smoothedY;
+
+            if (first)
+            {
+                smoothedY = point.OriginalY;
+                first = false;
+            }
+            else
+            {
+                smoothedY = alpha * point.OriginalY + (1 - alpha) * prevSmoothedY;
+            }
+
+            prevSmoothedY = smoothedY;
+            yield return point.WithExpSmoothedY(smoothedY);
+        }
+    }
+}
