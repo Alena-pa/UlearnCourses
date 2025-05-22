6899d91f3aaf1c6a5e967efddae4d9d9fd690aee
Alena-pa <alena.pakhnusheva@gmail.com>
2025-05-22 23:03:40 +0300

    Add VirtualMachine

--- VirtualMachine/VirtualMachine/Program.cs
+++ VirtualMachine/VirtualMachine/Program.cs
@@ -1,42 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
+using NUnitLite;
 
-namespace func.brainfuck
+namespace func.brainfuck;
+
+public class Program
 {
-    public class VirtualMachine : IVirtualMachine
-    {
-        public string Instructions { get; }
-        public int InstructionPointer { get; set; }
-        public byte[] Memory { get; }
-        public int MemoryPointer { get; set; }
+	private const string sierpinskiTriangleBrainfuckProgram = @"
+                                >
+                               + +
+                              +   -
+                             [ < + +
+                            +       +
+                           + +     + +
+                          >   -   ]   >
+                         + + + + + + + +
+                        [               >
+                       + +             + +
+                      <   -           ]   >
+                     > + + >         > > + >
+                    >       >       +       <
+                   < <     < <     < <     < <
+                  <   [   -   [   -   >   +   <
+                 ] > [ - < + > > > . < < ] > > >
+                [                               [
+               - >                             + +
+              +   +                           +   +
+             + + [ >                         + + + +
+            <       -                       ]       >
+           . <     < [                     - >     + <
+          ]   +   >   [                   -   >   +   +
+         + + + + + + + +                 < < + > ] > . [
+        -               ]               >               ]
+       ] +             < <             < [             - [
+      -   >           +   <           ]   +           >   [
+     - < + >         > > - [         - > + <         ] + + >
+    [       -       <       -       >       ]       <       <
+   < ]     < <     < <     ] +     + +     + +     + +     + +
+  +   .   +   +   +   .   [   -   ]   <   ]   +   +   +   +   +
+";
 
-        private readonly Dictionary<char, Action<IVirtualMachine>> commands;
-
-        public VirtualMachine(string program, int memorySize)
-        {
-            Instructions = program;
-            Memory = new byte[memorySize];
-            InstructionPointer = 0;
-            MemoryPointer = 0;
-            commands = new Dictionary<char, Action<IVirtualMachine>>();
-        }
-
-        public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
-        {
-            commands[symbol] = execute;
-        }
-
-        public void Run()
-        {
-            while (InstructionPointer >= 0 && InstructionPointer < Instructions.Length)
-            {
-                char currentInstruction = Instructions[InstructionPointer];
-                if (commands.ContainsKey(currentInstruction))
-                {
-                    commands[currentInstruction](this);
-                }
-                InstructionPointer++;
-            }
-        }
-    }
+	public static void Main(string[] args)
+	{
+		if (args.Contains("test"))
+			new AutoRun().Execute(new string[0]); // Запуск тестов
+		else
+		{
+			Brainfuck.Run(sierpinskiTriangleBrainfuckProgram, Console.Read, Console.Write);
+			Console.WriteLine("Это была демонстрация Brainfuck на примере построения треугольника Серпинского");
+		}
+		Console.ReadLine();
+	}
 }
